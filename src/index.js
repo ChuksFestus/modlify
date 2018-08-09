9358e2690c23966f6f440a8f4c4e875dd347004d
Chuks Festus <cutogu@gmail.com>
2018-08-09 10:14:41 +0100

    made it leaner

--- src/index.js
+++ src/index.js
@@ -6,24 +6,7 @@ import ModalContainer from "./ModalContainer/index";
 import "./styles.css";
 
 function App() {
-  return (
-    <React.Fragment>
-      <p>
-        Many desktop publishing packages and web page editors now use Lorem
-        Ipsum as their default model text, and a search for 'lorem ipsum' will
-        uncover many web sites still in their infancy. Various versions have
-        evolved over the years, sometimes by accident, sometimes on purpose
-        (injected humour and the like).
-      </p>
-      <ModalContainer buttonLabel="Open text modal">
-        <img
-          style={{ width: "100%" }}
-          src="https://placeimg.com/800/450/nature"
-          alt="Nature"
-        />
-      </ModalContainer>
-    </React.Fragment>
-  );
+  return <ModalContainer />;
 }
 
 const rootElement = document.getElementById("root");
